2c299d18c3f4605c042b1caa01a02ec588b3bd7a
Murilo Mattos <murilo.mattos@cscgfm.com>
2023-07-25 19:55:59 -0300

    added logout test case

--- pages/login_page.py
+++ pages/login_page.py
@@ -22,18 +22,18 @@ class LoginPage():
         return self.driver.find_elements(By.XPATH, '//h3[@data-test="error"]')
 
     # Page Actions
-    def type_user_name(self, user):
-        self.user_name_input().send_keys(user)
-
-    def type_password(self, password):
-        self.password_input().send_keys(password)
-
-    def click_login(self):
-        self.login_btn().click()
-
     def assert_login_error_message(self, text):
         error_messages = [element.text for element in self.login_error_messages()]
         assert text in error_messages
 
     def assert_user_name_field_is_visible(self):
         self.user_name_input().is_displayed()
+
+    def click_login(self):
+        self.login_btn().click()
+
+    def type_user_name(self, user):
+        self.user_name_input().send_keys(user)
+
+    def type_password(self, password):
+        self.password_input().send_keys(password)
